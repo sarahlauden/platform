cdd0f04f7a69a5f3c8f3c01bc637aa1c9027391d
rshipp <github@rshipp.com>
2019-10-22 10:36:04 -0500

    Rename and improve Platform entrypoint JS

--- app/javascript/packs/platform.js
+++ app/javascript/packs/platform.js
@@ -1,13 +1,14 @@
-// Run this example by adding <%= javascript_pack_tag 'development' %> to the head of your layout file,
+// This is the main entrypoint for Braven Portal.
+// Things that need to be set up globally go here.
+// Run this script by adding <%= javascript_pack_tag 'platform' %> to the head of your layout file,
 // like app/views/layouts/application.html.erb.
 
 import React from 'react'
 import ReactDOM from 'react-dom'
 
 // Use axe a11y testing in development.
-var axe = require('react-axe');
-
 if (process.env.NODE_ENV !== 'production') {
+  let axe = require('react-axe');
   document.addEventListener('DOMContentLoaded', () => {
     axe(React, ReactDOM, 1000);
   });
