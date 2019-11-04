a3e5f11869144982ab92d5682ec3ef044ceac6b3
rshipp <github@rshipp.com>
2019-11-04 13:53:19 -0600

    Fix type param name in controller

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -69,6 +69,6 @@ class ContentsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def content_params
-      params.require(:content).permit(:title, :body, :published_at, :type)
+      params.require(:content).permit(:title, :body, :published_at, :content_type)
     end
 end
