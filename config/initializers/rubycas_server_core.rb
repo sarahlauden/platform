d467f113114789ea819a0f33ce66002e58a17ef1
Brian Rose <stetsentech@gmail.com>
2019-11-12 04:40:55 -0500

    RubyCAS configs setup based on environment

--- config/initializers/rubycas_server_core.rb
+++ config/initializers/rubycas_server_core.rb
@@ -1,2 +1,2 @@
-cas_config = YAML.load_file("#{Rails.root.to_s}/config/rubycas.yml")
+cas_config = YAML.load_file("#{Rails.root.to_s}/config/rubycas.yml")[Rails.env]
 RubyCAS::Server::Core::setup(cas_config)
