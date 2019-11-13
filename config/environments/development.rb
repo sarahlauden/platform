bfcd5677e72e32907f6bba3ca77fede0f9a0eb3b
Brian Rose <stetsentech@gmail.com>
2019-11-13 12:28:31 -0500

    Moves logger from app level to environment level

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -68,4 +68,7 @@ Rails.application.configure do
 
   # Add Rack::LiveReload to the bottom of the middleware stack with the default options:
   config.middleware.insert_after ActionDispatch::Static, Rack::LiveReload
+
+  config.logger = Logger.new(STDOUT)
+
 end
