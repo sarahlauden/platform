099015011fa779d3ad097a9aeaf94743d71fa0c9
rshipp <github@rshipp.com>
2019-11-14 12:55:48 -0600

    Add new columns to course_content

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_14_174841) do
+ActiveRecord::Schema.define(version: 2019_11_14_183555) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,8 @@ ActiveRecord::Schema.define(version: 2019_11_14_174841) do
     t.string "content_type"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "course_id"
+    t.string "item_id"
   end
 
   create_table "emails", force: :cascade do |t|
