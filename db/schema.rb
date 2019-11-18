d5e1b715258e928080ac72aa3c688f7616229c9a
Brian Rose <stetsentech@gmail.com>
2019-11-18 10:15:35 -0500

    Updates DB Schema

--- db/schema.rb
+++ db/schema.rb
@@ -177,15 +177,17 @@ ActiveRecord::Schema.define(version: 2019_11_04_193832) do
     t.datetime "consumed"
     t.string "client_hostname", null: false
     t.string "username", null: false
-    t.string "type", null: false
     t.integer "proxy_granting_ticket_id"
-  end
+    t.integer "ticket_granting_ticket_id"
+end
 
   create_table "ticket_granting_tickets", force: :cascade do |t|
     t.string "ticket", null: false
     t.datetime "created_at", null: false
     t.string "client_hostname", null: false
     t.string "username", null: false
+    t.string "remember_me", null: false
+    t.string "extra_attributes", null: false
   end
 
   create_table "users", force: :cascade do |t|
