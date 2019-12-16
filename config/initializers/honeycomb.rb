6586fc4d7e9f4140c31c6ce30151b4cf123c5b35
Brian Rose <stetsentech@gmail.com>
2019-12-16 11:19:09 -0500

    Adds require

--- config/initializers/honeycomb.rb
+++ config/initializers/honeycomb.rb
@@ -1,3 +1,5 @@
+require "honeycomb-beeline"
+
 Honeycomb.configure do |config|
   config.write_key = Rails.application.secrets.honeycomb_api_key
   config.dataset = Rails.application.secrets.honeycomb_dataset
