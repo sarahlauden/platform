92d95d2fbebafafdb2dcb6a07abc2306cccb2f15
Brian Rose <stetsentech@gmail.com>
2019-12-16 11:50:08 -0500

    Adds code so Honeycomb is optional

--- config/initializers/honeycomb.rb
+++ config/initializers/honeycomb.rb
@@ -1,6 +1,11 @@
-require "honeycomb-beeline"
+if Gem.loaded_specs.has_key?('honeycomb-beeline')
+  Rails.logger.info "Honeycomb detected. Initalizing setup."
+  require "honeycomb-beeline"
 
-Honeycomb.configure do |config|
-  config.write_key = Rails.application.secrets.honeycomb_write_key
-  config.dataset = Rails.application.secrets.honeycomb_dataset
+  Honeycomb.configure do |config|
+    config.write_key = Rails.application.secrets.honeycomb_write_key
+    config.dataset = Rails.application.secrets.honeycomb_dataset
+  end
+else
+  Rails.logger.warn "Honeycomb is not install. Skipping initialization."
 end
