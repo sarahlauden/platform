128631104b1fc62693cc7bf1969dc388484346c8
Brian Rose <stetsentech@gmail.com>
2019-12-17 10:13:15 -0500

    Wraps init with check for env

--- config/initializers/honeycomb.rb
+++ config/initializers/honeycomb.rb
@@ -1,11 +1,13 @@
-if Gem.loaded_specs.has_key?('honeycomb-beeline')
-  Rails.logger.info "Honeycomb detected. Initalizing setup."
-  require "honeycomb-beeline"
+if Rails.application.secrets.honeycomb_write_key && Rails.application.secrets.honeycomb_dataset
+  if Gem.loaded_specs.has_key?('honeycomb-beeline')
+    require "honeycomb-beeline"
 
-  Honeycomb.configure do |config|
-    config.write_key = Rails.application.secrets.honeycomb_write_key
-    config.dataset = Rails.application.secrets.honeycomb_dataset
+    Rails.logger.info "Honeycomb Beeline detected. Initalizing setup."
+    Honeycomb.configure do |config|
+      config.write_key = Rails.application.secrets.honeycomb_write_key
+      config.dataset = Rails.application.secrets.honeycomb_dataset
+    end
+  else
+    Rails.logger.warn "Honeycomb Beeline is not install. Skipping initialization."
   end
-else
-  Rails.logger.warn "Honeycomb is not install. Skipping initialization."
 end
